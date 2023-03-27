440e15ac018fb91c5e019e1316358190422b5b2d
Dominic Yuen <dominicy@orionhealth.com>
2023-03-28 02:11:37 +1300

    Added table for displaying notifications

--- src/PageComponents/Notifications/CreateNotifications.tsx
+++ src/PageComponents/Notifications/CreateNotifications.tsx
@@ -1,31 +1,77 @@
 import { useState } from 'react'
 
 import { Layout, Input, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import { ViewNotifications, DataType } from './ViewNotifications';
 
 const { TextArea } = Input;
 
 export function CreateNotification() {
     const [notificationText, setNotificationText] = useState("")
-  
+    const [author, setAuthor] = useState("")
+
+    const [ tableData, setTableData ] = useState<DataType[]>([])
+
+    function addNewRecord() {
+      setTableData([...tableData, {
+        key: new Date(Date.now()).toLocaleString(),
+        dateCreated: new Date(Date.now()).toLocaleString(),
+        notification: notificationText,
+        author: author,
+        status: 'Active',
+      }])
+      setAuthor("")
+      setNotificationText("")
+    }
+
     return (
       <>
-        <TextArea
-          placeholder="Write your Notification here . . ."
-          autoSize={{ minRows: 5, maxRows: 30 }}
-          value={notificationText}
-          showCount
-          maxLength={500}
-          onChange={(e) => {setNotificationText(e.target.value)}}
-        />
-        <Button 
+        <div style={{marginBottom: "20px"}}>
+          <TextArea
+            placeholder="Write your Notification here . . ."
+            autoSize={{ minRows: 5, maxRows: 30 }}
+            value={notificationText}
+            showCount
+            maxLength={500}
+            onChange={(e) => {setNotificationText(e.target.value)}}
+          />
+          <div>
+            <div style={{
+              display: "inline-block",
+              width: "60px",
+              paddingLeft: 0,
+              paddingRight: 10,
+              paddingTop: 20,
+              fontWeight: 'bold'              
+              }}>
+                Author:
+            </div>
+              <Input 
+                placeholder="default size" 
+                prefix={<UserOutlined />} 
+                style={{ width: "250px"}}
+                onChange={(e) => {setAuthor(e.target.value)}}
+                value={author}
+              />
+            <div>
+            </div>
+
+          </div>
+
+          <Button 
           type="primary"
-          onClick={undefined}
+          onClick={() => addNewRecord()}
           style={{
-            margin: "10px"
+              margin: "10px"
           }}
           >
-            Create Notification
-        </Button>
+              Create Notification
+          </Button>
+        </div>
+
+
+        <ViewNotifications tableData={tableData}/>
       </>
     )
   }
--- src/PageComponents/Notifications/ViewNotifications.tsx
+++ src/PageComponents/Notifications/ViewNotifications.tsx
@@ -1,1 +1,102 @@
-// Placeholder
+import { useState } from 'react'
+
+import { Space, Input, Button, Table } from 'antd';
+
+import type { ColumnsType } from 'antd/es/table';
+
+const { TextArea } = Input;
+
+
+export interface DataType {
+  key: string;
+  dateCreated: string;
+  notification: string;
+  author: string;
+  status: string;
+}
+  
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Date Created',
+    dataIndex: 'dateCreated',
+    key: 'dateCreated',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Author',
+    dataIndex: 'author',
+    key: 'author',
+  },
+  {
+    title: 'Notification',
+    dataIndex: 'notification',
+    key: 'notification',
+  },
+  {
+    title: 'Status',
+    key: 'status',
+    dataIndex: 'status',
+  //   render: (_: any, { tags }) => (
+  //     <>
+  //       {tags.map((tag) => {
+  //         let color = tag.length > 5 ? 'geekblue' : 'green';
+  //         if (tag === 'loser') {
+  //           color = 'volcano';
+  //         }
+  //         return (
+  //           <Tag color={color} key={tag}>
+  //             {tag.toUpperCase()}
+  //           </Tag>
+  //         );
+  //       })}
+  //     </>
+  //   ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_: any, record: any) => (
+      <Space size="middle">
+        <a>Edit</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    dateCreated: '27 March 2023',
+    notification: 'Time to pray',
+    author: 'Dom Yuen',
+    status: 'Active',
+  },
+  {
+    key: '2',
+    dateCreated: '27 March 2023',
+    notification: 'Time to pray',
+    author: 'Dom Yuen',
+    status: 'Active',
+  },
+  {
+    key: '3',
+    dateCreated: '27 March 2023',
+    notification: 'Time to pray',
+    author: 'Dom Yuen',
+    status: 'Active',
+  },
+];
+
+interface TableData {
+  tableData: DataType[];
+}
+export function ViewNotifications(props:TableData) {
+    // const [notificationText, setNotificationText] = useState("")
+  
+    return (
+      <>
+        <Table columns={columns} dataSource={props.tableData} size={"small"}/>
+      </>
+    )
+  }
